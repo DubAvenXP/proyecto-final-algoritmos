26f3f09058223c6f30cc4d8050c3fa22b5c7b801
Alejandro Dubon <alejandrodubon88@gmail.com>
2020-08-09 09:56:23 -0600

    generando la primera parte de componentes

--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShoppingRoutingModule } from './shopping-routing.module';
+import { ShoppingComponent } from './components/shopping/shopping.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ShoppingComponent],
   imports: [
     CommonModule,
     ShoppingRoutingModule
